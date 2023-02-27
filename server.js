f50f4129e10889e02a5c29ce85cfbbbcd8a45ce7
Alt B 2022 <massie.w@gmail.com>
2023-02-26 21:50:46 -0800

    Added content to server.js for query, default response and app.listen PORT. Created .env file and added to .gitignore

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ const PORT = process.env.PORT || 3001;
 const db = mysql.createConnection(
   {
     host: 'localhost',
- 
     user: 'root',
-
     password: 'PASSWORD',
     database: 'employee_db'
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+db.query('SELECT * FROM employees', function (err, results) {
+    console.log(results);
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
