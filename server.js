8d0cddc0531c9aa6e57f5fc069acb5c784bc423e
Alt B 2022 <massie.w@gmail.com>
2023-02-27 10:43:21 -0800

    Reworked connection to db and got it functioning.  Added function menu() with .then answer => switch case and added all scenarios.  Wrote function to showAllDepartments() and successfully tested functionality.  Can now write all remaining functions.

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-// const express = require('express');
-
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table')
 
-const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -20,14 +14,62 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-db.query('SELECT * FROM employees', function (err, results) {
-    console.log(results);
-});
+db.connect(function(err){
+    if(err)throw err;
+    menu();
+}
+)
+// db.query('SELECT * FROM employees', function (err, results) {
+//     console.log(results);
+// });
 
-app.use((req, res) => {
-    res.status(404).end();
-});
+function menu() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "options",
+            message: "Which option would you like to select",
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "add an employee role"]
+        }).then((answer)=>{
+            switch(answer.options){
+                case "view all departments":
+                    showAllDepartments();
+                    console.log('hello');
+                    break;
+                case "view all roles":
+                    viewAllRoles();
+                    break;
+                case "view all employees":
+                    viewAllEmployees();
+                    break;
+                case "add a department":
+                    addDepartment();
+                    break;
+                case "add a role":
+                    AddRole();
+                    break;
+                case "add an employee":
+                    addEmployee();
+                    break;
+                case "add an employee role":
+                    addEmployeeRole();
+                    break;
+                case "Quit":
+                    quitApp();
+                    break;
+                default:
+                    quitApp();
+            }
+        })
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+function showAllDepartments(){
+    let query = 'SELECT * from department';
+    db.query(query, function(err, res){
+        if(err)throw err;
+        console.table('Departments', res);
+        menu();
+    })
+}
+
+
