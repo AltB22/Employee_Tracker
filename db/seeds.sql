440fe49aa14fba78649554a1eed7ade0999e191f
Alt B 2022 <massie.w@gmail.com>
2023-02-26 21:37:05 -0800

    table data bug fixes

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,16 +12,16 @@ VALUES (1, "Head of Sales", 125000, 1),
        (5, "Account Manager", 170000, 3),
        (6, "Accountant", 120000, 3),
        (7, "Lead Counsel", 225000, 4),
-       (8, "Lawyer", 170000, 4;
+       (8, "Lawyer", 170000, 4);
 
 INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "Steve", "Harrison", 1, NULL),
-       (2, "Tim", "Burrows" 2, 1),
-       (3, "William", "Massie" 3, NULL),
+       (2, "Tim", "Burrows", 2, 1),
+       (3, "William", "Massie", 3, NULL),
        (4, "Sarah", "Peters", 4, 3),
        (5, "Ashley", "Smith", 5, NULL),
        (6, "John", "Murphy", 6, 5),
-       (7, "David", "Lacosta",7, NULL),
+       (7, "David", "Lacosta", 7, NULL),
        (8, "Christine", "Murray", 8, 7);
 
 
