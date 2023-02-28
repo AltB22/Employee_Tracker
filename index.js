284b2777b95171253c2d50be6886702f8b278cf4
Alt B 2022 <massie.w@gmail.com>
2023-02-27 23:58:34 -0800

    Finshed out viewAllEmployees function and addDepartment function in index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-require('console.table')
+const consoleTable = require('console.table')
 
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'PASSWORD',
-    database: 'employee_db'
-  },
-  console.log(`Connected to the employee_db database.`)
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'PASSWORD',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
 );
 
-db.connect(function(err){
-    if(err)throw err;
+db.connect(function (err) {
+    if (err) throw err;
     menu();
 }
 )
@@ -30,8 +30,8 @@ function menu() {
             name: "options",
             message: "Which option would you like to select",
             choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "add an employee role", "Quit"]
-        }).then((answer)=>{
-            switch(answer.options){
+        }).then((answer) => {
+            switch (answer.options) {
                 case "view all departments":
                     showAllDepartments();
                     console.log('hello');
@@ -63,53 +63,58 @@ function menu() {
         })
 }
 
-function showAllDepartments(){
+function showAllDepartments() {
     let query = 'SELECT * from department';
-    db.query(query, function(err, res){
-        if(err)throw err;
+    db.query(query, function (err, res) {
+        if (err) throw err;
         console.table('Departments', res);
         menu();
     })
 }
 
-function viewAllRoles(){
+function viewAllRoles() {
     // let query = 'SELECT * from role';
     let query = 'SELECT role.title, role.id AS role_id, department.name AS department_name, role.salary FROM role JOIN department ON role.department_id = department.id'
 
-
-
-
-    db.query(query, function(err, res){
-        if(err)throw err;
+    db.query(query, function (err, res) {
+        if (err) throw err;
         console.table('Roles', res);
         menu();
     })
 }
 
-function viewAllEmployees(){
-    let query = 'SELECT * from employee';//look at join statments
-    db.query(query, function(err, res){
-        if(err)throw err;
+function viewAllEmployees() {
+    let query = 'SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department_name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager_name FROM employee employee JOIN role role ON employee.role_id = role.id JOIN department department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;'
+
+    db.query(query, function (err, res) {
+        if (err) throw err;
         console.table('Employees', res);
         menu();
     })
 }
 
-function addDepartment(){
+function addDepartment() {
     inquirer.prompt(
         {
             type: 'input',
             name: 'newDepartment',
             message: "Please enter the department you would you like to add."
         }
-    ).then((answer)=>{
+    ).then((answer) => {
         db.query(
-            'INSERT INTO department',
+            'INSERT INTO department SET ?',
             {
-                name: answer.newDepartment,
+                name: answer.newDepartment
             });
+        let query = 'SELECT * FROM department';
+        db.query(query, function (err, res) {
+            if (err) throw err;
+            console.log('New department has been successfully added');
+            console.table('Departments:', res);
             menu();
-        });
-        
-    };
+        })
+
+    });
+
+};
 
