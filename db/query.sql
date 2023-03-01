83edff6ba07b1286dfbd81f7e8b93806fadf5997
Alt B 2022 <massie.w@gmail.com>
2023-02-28 22:42:34 -0800

    Added minor content to direct the user in prompts

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,4 @@
-SELECT role.title AS title, role.id, role.salary FROM role
-JOIN department.name ON role.department_id = department.id
+--SELECT role.title AS title, role.id, role.salary FROM role
+--JOIN department.name ON role.department_id = department.id
+
+--This file was deprecated.  Leaving for potential future use
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//requires mysql2, inquirer & console.table to be installed (npm i)
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
 
-// Connect to database
+// Connect to database, will be referred as db.x hereafter
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -14,11 +15,13 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
+//callback function to throw error if error or call menu() function
 db.connect(function (err) {
     if (err) throw err;
     menu();
 });
 
+//menu function to serve as main menu of list options for to choose actions to be taken.  Depending on the selection made, switch / case statment below will direct user to the appropriate function.
 function menu() {
     inquirer.prompt(
         {
@@ -62,6 +65,7 @@ function menu() {
         });
 };
 
+//Displays all departments with a simple select all from departments query.  Displays using console.table labeled as "Departments"
 function showAllDepartments() {
     let query = 'SELECT * from department';
     db.query(query, function (err, res) {
@@ -71,8 +75,8 @@ function showAllDepartments() {
     });
 };
 
+//Creates a hybrid table displaying role title, role id, and role salary from role table, and joins department name wherever department ids match in the two tables.  
 function viewAllRoles() {
-    // let query = 'SELECT * from role';
     let query = 'SELECT role.title, role.id AS role_id, department.name AS department_name, role.salary FROM role JOIN department ON role.department_id = department.id'
 
     db.query(query, function (err, res) {
@@ -82,6 +86,7 @@ function viewAllRoles() {
     });
 };
 
+//Creates a custom / hybrid table to display the data as laid out by the acceptance criteria.  This took some serious google searching and research as well as guidance from tutor and about 50x with errors before it finally worked!! I had to build a spreadsheet with all the table colums laid out to visualize it and then make selections in the sheet and transferred into squl query syntax.  Would have preferred to utilize the query.sql file for all of these but it works this way and need to move on!!  Also displays the results via console.table as "Employees".
 function viewAllEmployees() {
     let query = 'SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department_name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager_name FROM employee employee JOIN role role ON employee.role_id = role.id JOIN department department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;'
 
@@ -92,6 +97,7 @@ function viewAllEmployees() {
     });
 };
 
+//Gives the user ability to input a new department.  Auto Increment feature will automatically assign a department number to new departments.  Displays updated department table as "Departments" and console logs successful add.
 function addDepartment() {
     inquirer.prompt(
         {
@@ -109,7 +115,7 @@ function addDepartment() {
         db.query(query, function (err, res) {
             if (err) throw err;
             console.log('New department has been successfully added');
-            // console.table('Departments:', res);
+            console.table('Departments:', res);
             menu();
         })
 
@@ -117,6 +123,7 @@ function addDepartment() {
 
 };
 
+//Gives the user the ability to add a role including salary and which dept to assign to and displays updated table afterwards as well as confirmation via console log of adding the role.
 function addRole() {
     inquirer.prompt([
         {
@@ -132,7 +139,7 @@ function addRole() {
         {
             type: 'input',
             name: 'newRoleDept',
-            message: "Please enter the department of the role you would you like to add."
+            message: "Please enter the department id of the role you would you like to add (must be a number)"
         },
     ]).then((answer) => {
         db.query(
@@ -154,6 +161,7 @@ function addRole() {
 
 }
 
+//Gives the user ability to add a new employee assigning all the require table column data per schema. (auto-increment will assign the id)
 function addEmployee() {
     inquirer.prompt([
         {
@@ -174,7 +182,7 @@ function addEmployee() {
         {
             type: 'input',
             name: 'newEmployeeManager',
-            message: "Please enter the manager id for the new employee you would like to add."
+            message: "Please enter the manager id for the new employee you would like to add. (must be a number)"
         },
     ]).then((answer) => {
         db.query(
@@ -197,6 +205,7 @@ function addEmployee() {
 
 };
 
+//Gives the user abiltiy to delete an employee from database.
 function deleteEmployee() {
     db.query('SELECT id, CONCAT(first_name, " ", last_name) AS name from employee',
         (err, res) => {
@@ -223,7 +232,7 @@ function deleteEmployee() {
         });
 };
 
-
+//Gives user ability to assign a new role to an employee.  Multiple employees may be assigned to the same role (ie more than 1 salesperson...).  
 function addEmployeeRole() {
     db.query('SELECT id, CONCAT(first_name, " ", last_name) AS name from employee',
         (err, res) => {
@@ -239,7 +248,7 @@ function addEmployeeRole() {
                 {
                     type: 'input',
                     name: 'newEmployeeRole',
-                    message: "Please enter the role id of the employee's new role.",
+                    message: "Please enter the role id of the employee's new role. (must be a number)",
                 }
             ]).then((answer) => {
                 let employeeId = answer.employeeName;
@@ -259,7 +268,7 @@ function addEmployeeRole() {
         });
 };
 
-
+//Exits the application
 function quitApp() {
     db.end();
     console.log("Good-Bye");
